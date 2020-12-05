9817aa84e6d25158ece57af948be59ae6341bb02
f2015712 <sks2971996@gmail.com>
2020-12-05 11:48:15 +0530

    modified code

--- valid-parentheses.py
+++ valid-parentheses.py
@@ -1,23 +1,20 @@
 class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        start=0
-        n=len(s)
-        if n==0 or n==1:
-            return n
-        max_len=-10000000
-        
-        while start<n:
-            #print(start)
-            count=0
-            visited={}
-            for i in range(start,n):
-                if s[i] not in visited:
-                    count+=1
-                    visited[s[i]]=1
-                    max_len=max(max_len,count)
+    def isValid(self, s):
+        stack=[]
+        for i in range(len(s)):
+            if s[i] in ['(','[','{'] or len(stack)==0:
+                stack.append(s[i])
+            else:
+                if s[i]==')' and stack[-1]=='(':
+                    stack.pop()
+                elif s[i]==']' and stack[-1]=='[':
+                    stack.pop()
+                elif s[i]=='}' and stack[-1]=='{':
+                    stack.pop()
                 else:
-                    max_len=max(max_len,count)
-                    
-                    break
-            start+=1
-        return max_len
+                    stack.append(s[i])
+        #print(len(stack))
+        if len(stack)==0:
+            return True
+        return False
+        
